b167d4a90d97393ab8705d2f401aacb309768eb7
yarakigit <ya.tech.go@gmail.com>
2022-06-05 16:20:29 +0900

    big change

---
+++ scripts/build_site/pelicanconf.py
@@ -0,0 +1,37 @@
+AUTHOR = 'youtube ranking'
+SITENAME = 'youtube ranking'
+#SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'https://getpelican.com/'),
+#         ('Python.org', 'https://www.python.org/'),
+#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 7
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# my setting
+THEME = './flex'
+
+DATE_FORMATS = {
+    'ja': '%Y-%m-%d',
+}
