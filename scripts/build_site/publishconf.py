b6104fa9213f63c254a75d30b76dbee37f9edd82
yarakigit <ya.tech.go@gmail.com>
2022-06-05 18:16:31 +0900

    l

--- scripts/build_site/publishconf.py
+++ scripts/build_site/publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://yarakigit.github.io/sample-blog-pelican/'
+SITEURL = 'https://yarakigit.github.io/youtube_ranking_build/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
